2cbf5e17cf0e7177265a7c99ca95dd0ad03e73f3
luisriego <luisriego@hotmail.com>
2024-01-26 20:33:36 -0300

    Installed Symfony 6.4 add phpunit add PHP-CS-Fixer add PHPStan add PHPat

---
+++ tests/PHPat/ArchitectureTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Test\PHPat;
+
+use PHPat\Selector\Selector;
+use PHPat\Test\Builder\Rule;
+use PHPat\Test\PHPat;
+
+final class ArchitectureTest
+{
+    public function testDomainDoesNotDependsOnApplicationAndAdapter(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::inNamespace('App\Domain'))
+            ->shouldNotDependOn()
+            ->classes(
+                Selector::inNamespace('App\Application'),
+                Selector::inNamespace('App\Adapter'),
+            );
+    }
+
+    public function testApplicationDoesNotDependsOnAdapter(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::inNamespace('App\Application'))
+            ->shouldNotDependOn()
+            ->classes(
+                Selector::inNamespace('App\Adapter'),
+            );
+    }
+}
