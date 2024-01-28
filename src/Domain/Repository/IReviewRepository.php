84c69179a83997a29421443f7a72168ec455ef9b
luisriego <luisriego@hotmail.com>
2024-01-28 18:38:48 -0300

    little changes in IReviewRepository

--- src/Domain/Repository/IReviewRepository.php
+++ src/Domain/Repository/IReviewRepository.php
@@ -9,10 +9,7 @@ use App\Domain\Model\Review;
 interface IReviewRepository
 {
     public function add(Review $review, bool $flush): void;
-
     public function save(Review $review, bool $flush): void;
-
     public function remove(Review $review, bool $flush): void;
-
     public function findOneByIdOrFail(string $id): Review;
 }
