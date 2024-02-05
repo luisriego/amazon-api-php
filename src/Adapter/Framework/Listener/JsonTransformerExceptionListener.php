aa4418c86a1d9a16afd67e9a1c7ab55cf48671f1
luisriego <luisriego@hotmail.com>
2024-02-05 19:30:06 -0300

    pre configure the event subscriber

---
+++ src/Adapter/Framework/Listener/JsonTransformerExceptionListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Adapter\Framework\Listener;
+
+use App\Domain\Exception\InvalidArgumentException;
+use App\Domain\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class JsonTransformerExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $e = $event->getThrowable();
+
+        $data = [
+            'class' => \get_class($e),
+            'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+            'message' => $e->getMessage(),
+        ];
+
+        if ($e instanceof ResourceNotFoundException) {
+            $data['code'] = Response::HTTP_NOT_FOUND;
+        }
+
+        if ($e instanceof InvalidArgumentException) {
+            $data['code'] = Response::HTTP_BAD_REQUEST;
+        }
+
+//        if ($e instanceof AccessDeniedException) {
+//            $data['code'] = Response::HTTP_FORBIDDEN;
+//        }
+
+        $response = new JsonResponse($data, $data['code']);
+
+        $event->setResponse($response);
+    }
+}
