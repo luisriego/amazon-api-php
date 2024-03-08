72fea556847f9427abe34568f62d85f98397b48f
luisriego <luisriego@hotmail.com>
2024-03-08 12:20:47 -0300

    Solved problem with activation status

---
+++ src/Adapter/Framework/Security/EventListener/JWTCreatedListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Adapter\Framework\Security\EventListener;
+
+use App\Domain\Model\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+readonly class JWTCreatedListener
+{
+
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        /** @var User $user */
+        $user = $event->getUser();
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
+    }
+}
